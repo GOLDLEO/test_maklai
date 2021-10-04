dd6e2172482400288a088f20fd16f5ed3d60cb93
mnd <oceanven@gmail.com>
2021-10-04 06:32:04 +0300

    add validator, update view, add language check

--- speach/forms.py
+++ speach/forms.py
@@ -1,4 +1,23 @@
-from django import forms 
+from django import forms
+from django.core.exceptions import ValidationError
+from langdetect import detect
+
 
 class MainForm(forms.Form):
-	free_text_field = forms.CharField(label='Your text', widget=forms.Textarea)
+	free_text_field = forms.CharField(
+		label='Your text',
+		widget=forms.TextInput(
+			attrs={'class': 'form-control', 'style': 'margin: 50px;'}
+		)
+	)
+
+	def clean_free_text_field(self):
+		cleaned_data = super(MainForm, self).clean()
+		data = self.cleaned_data.get('free_text_field')
+		if detect(data) != 'en':
+			print('no eng')
+			raise ValidationError(
+				('Please use only English.'),
+				code='invalid',
+			)
+		return data
