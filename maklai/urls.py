4c9a896dd38736f5042f416471e6eeb4bf4d672f
mnd <oceanven@gmail.com>
2021-10-04 06:42:11 +0300

    add templates and etc

--- maklai/urls.py
+++ maklai/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('speach/', include('speach.urls')),
+    path('', include('speach.urls')),
     
 ]
--- speach/views.py
+++ speach/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import MainForm
 from .models import Text, DetailText
@@ -6,23 +6,34 @@ import spacy
 
 # Create your views here.
 
+
 def index(request):
+	print(1)
 	if request.method == 'POST':
+		print(2)
 		form = MainForm(request.POST)
 		if form.is_valid():
-			text = form.cleaned_data['free_text_field']
+			text = form.cleaned_data.get('free_text_field',)
 			nlp = spacy.load("en_core_web_sm")
 			doc = nlp(str(text))
 			field_to_db = Text(full_text=text)
 			field_to_db.save()
 			for token in doc:
-				fields_to_db = DetailText(text=str(token.text), pos=str(token.pos_), tag=str(token.tag_),text_id=field_to_db)
+				fields_to_db = DetailText(
+					text=str(token.text),
+					pos=str(token.pos_),
+					tag=str(token.tag_),
+					text_id=field_to_db)
 				fields_to_db.save()
-			
-			return HttpResponse('success')
-	else:			
-		form = MainForm()
-		return render(request, 'index.html', {'form':form})
+			return redirect(output_view)
+		else:
+			msg = form.errors.as_data()['free_text_field'][0]
+			print(msg)
+			return render(request, 'index.html', {'form': form, 'errors': msg})
+
+	form = MainForm()
+	return render(request, 'index.html', {'form': form})
+
 
 def output_view(request):
 	query = Text.objects.all()
@@ -30,7 +41,6 @@ def output_view(request):
 	for q in query:
 		values=[]
 		fields = DetailText.objects.all().filter(text_id=q.pk)
-		print(fields)
 		for f in fields: 
 			if f.pos == 'PUNCT':
 				pass
